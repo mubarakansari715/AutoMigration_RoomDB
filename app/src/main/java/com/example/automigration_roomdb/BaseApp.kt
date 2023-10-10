45f59b4a8a8c999ff5424d97864ffe9aa58327f0
mubarakbacancy <mubarak.ansari@bacancy.com>
2023-10-10 17:00:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/automigration_roomdb/BaseApp.kt
@@ -0,0 +1,12 @@
+package com.example.automigration_roomdb
+
+import android.app.Application
+
+class BaseApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
